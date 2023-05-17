fc073a6a59096cc76389ff4368b52b2ccf30d7f8
Vinogradov Artem <vinogradov44@outlook.com>
2023-05-17 15:33:50 +0300

    TODO: HOMEWORK разобраться

--- src/test/java/guru/qa/homework/CheckingFilesInArchiveTest.java
+++ src/test/java/guru/qa/homework/CheckingFilesInArchiveTest.java
@@ -4,7 +4,6 @@ import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import guru.qa.SelenideFilesTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -20,11 +19,9 @@ public class CheckingFilesInArchiveTest {
 
     ClassLoader cl = SelenideFilesTest.class.getClassLoader();
 
-    String[][] files = {{"cities.csv"}, {"file_example_XLS_10.xls"}, {"junit-user-guide-5.9.3.pdf"}};
-
     @ParameterizedTest
-    @CsvSource({"cities.csv", "file_example_XLS_10.xls", "junit-user-guide-5.9.3.pdf"})
-    void checkingFilesInArchiveTest(String fileName) throws Exception {
+    @CsvSource({".csv", ".xls", ".pdf"})
+    void checkingFilesInArchiveTest(String extendsFile) throws Exception {
         try (
                 InputStream resourceAsStream = cl.getResourceAsStream("hw.zip");
                 ZipInputStream zis = new ZipInputStream(resourceAsStream)
@@ -32,30 +29,23 @@ public class CheckingFilesInArchiveTest {
         ) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
-
-                switch (fileName) {
-                    case entry.getName() -> {
-                        CSVReader reader = new CSVReader(new InputStreamReader(zis));
-                        List<String[]> content = reader.readAll();
-                        assertThat(content.get(1)[8]).contains("Youngstown");
-
-                    }
-                    case fileName -> {
-                        PDF pdfContent = new PDF(zis);
-                        assertThat(pdfContent.author).contains("Sam Brannen");
-                    }
-                    case ".xls" -> {
-                        XLS xls = new XLS(zis);
-                        assertThat(xls.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue()).contains("Dulce");
-                    }
-                    default -> throw new IllegalStateException("Unexpected value: " + entry.getName());
+                if (entry.getName().endsWith(extendsFile)) {
+                    CSVReader reader = new CSVReader(new InputStreamReader(zis));
+                    List<String[]> content = reader.readAll();
+                    assertThat(content.get(1)[8]).contains("Youngstown");
                 }
-
+                if (entry.getName().endsWith(extendsFile)) {
+                    PDF pdfContent = new PDF(zis);
+                    assertThat(pdfContent.author).contains("Sam Brannen");
+                }
+                if (entry.getName().endsWith(extendsFile)) {
+                    CSVReader reader = new CSVReader(new InputStreamReader(zis));
+                    XLS xls = new XLS(zis);
+                    assertThat(xls.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue()).contains("Dulce");
+                }
 
             }
         }
-
-
     }
 }
 
